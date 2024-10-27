5f26d057be320fc60de8d69f1f7af8be8df1080f
Oleksandr <vt211_vos@student.ztu.edu.ua>
2024-10-27 17:19:05 +0200

    add prime vue

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,12 +2,19 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config';
+import Lara from '@primevue/themes/lara';
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara
+  }
+});
 app.use(createPinia())
 app.use(router)
 
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,7 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Button from "primevue/button"
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <Button/>
 </template>
